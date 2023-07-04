8a24460c92691d16cccf2c77ba4ff0f93054a674
Matheus Melo <65732203+melomatheuss@users.noreply.github.com>
2023-07-04 17:47:45 -0400

    Add files via upload

---
+++ E-commerce/order-service/index.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT_ONE || 9090;
+const mongoose = require("mongoose");
+const Order = require("./Order");
+const amqp = require("amqplib");
+
+var channel, connection;
+
+// Conecta ao banco de dados usando async/await
+async function connectToDatabase() {
+    try {
+      await mongoose.connect('mongodb://localhost/order-service', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+      console.log('Conexão Order-Service com o banco de dados estabelecida com sucesso!');
+    } catch (error) {
+      console.error('Erro ao conectar ao banco de dados:', error);
+    }
+  }
+  connectToDatabase();
+
+app.use(express.json());
+
+function createOrder(products, userEmail) {
+    let total = 0;
+    for (let t = 0; t < products.length; ++t) {
+        total += products[t].price;
+    }
+    const newOrder = new Order({
+        products,
+        user: userEmail,
+        total_price: total,
+    });
+    newOrder.save();
+    return newOrder;
+}
+
+async function connect() {
+    const amqpServer = "amqp://localhost:5672";
+    connection = await amqp.connect(amqpServer);
+    channel = await connection.createChannel();
+    await channel.assertQueue("ORDER");
+}
+connect().then(() => {
+    channel.consume("ORDER", (data) => {
+        console.log("Consuming ORDER service");
+        const { products, userEmail } = JSON.parse(data.content);
+        const newOrder = createOrder(products, userEmail);
+        channel.ack(data);
+        channel.sendToQueue(
+            "PRODUCT",
+            Buffer.from(JSON.stringify({ newOrder }))
+        );
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Order-Service at ${PORT}`);
+});
